e71614d3e6605b1c11b08b1838b2fffa89016301
fanyCaz <stepheniecaz@gmail.com>
2020-12-29 22:23:14 -0600

    erase last proy

--- app/controllers/application_controller.rb
+++
@@ -1,9 +0,0 @@
-class ApplicationController < ActionController::Base
-    def hello
-        render html: "hellloooo"
-    end
-
-    def bai
-        render html: "i say baibaibai"
-    end
-end
--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  #controller # nombre funcion
-  root 'application#hello'
-
-  get '/bai', to: 'application#bai'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
