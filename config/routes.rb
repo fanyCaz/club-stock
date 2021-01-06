b51bfe9290da57c8132b08009a0782e9149d455a
fanyCaz <stepheniecaz@gmail.com>
2021-01-06 16:20:43 -0600

    home page setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   #Set the controller products to be the root
-  root "products#index"
+  root "static_pages#home"
   #GET method , request to /products are handled in the index function of the Products Controller
   resources :products
   #get "/products", to: "products#index"
