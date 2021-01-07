477815b4aa4d181898cc1f734192302d021610c6
fanyCaz <stepheniecaz@gmail.com>
2021-01-07 11:22:32 -0600

    test files and contact page setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
+  
+  #STATICS
   get 'static_pages/home'
+  get 'static_pages/about'
+  get 'static_pages/contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   #Set the controller products to be the root
   root "static_pages#home"
   #GET method , request to /products are handled in the index function of the Products Controller
   resources :products
+  #'resources' gets all the product urls
   #get "/products", to: "products#index"
   #get "/products/:id", to: "products#show"
 end
