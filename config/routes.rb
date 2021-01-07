5041c9af30770b7bc9d6da99ed85091374f9af16
fanyCaz <stepheniecaz@gmail.com>
2021-01-07 15:19:42 -0600

    changed names of static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  
+  root "static_pages#home"
   #STATICS
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get "/home", to: "static_pages#home"
+  get "/about", to: "static_pages#about"
+  get '/contact', to: "static_pages#contact"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  #Set the controller products to be the root
-  root "static_pages#home"
   #GET method , request to /products are handled in the index function of the Products Controller
   resources :products
   #'resources' gets all the product urls
